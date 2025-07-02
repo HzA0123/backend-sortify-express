fc39e2f0a8500fd196d6239f81590314f8069164
Hafid Zayyan Ali <hafidzayyan21@gmail.com>
2025-07-02 18:05:20 +0700

    memperbarui index.js

--- models/index.js
+++ models/index.js
@@ -7,18 +7,23 @@ const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const db = {};
 
-// Buat instance koneksi manual dari .env
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOSTNAME,
-    dialect: "mysql",
-    port: 3306,
-    logging: false, // Optional: matikan logging SQL
-  }
-);
+// Buat instance koneksi manual dari .env atau DATABASE_URL Railway
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "mysql",
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USERNAME,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOSTNAME,
+        dialect: "mysql",
+        port: 3306,
+        logging: false, // Optional: matikan logging SQL
+      }
+    );
 
 // Load semua model dari folder ini
 fs.readdirSync(__dirname)
